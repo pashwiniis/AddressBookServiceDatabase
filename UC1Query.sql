009f8b26ddc49d406606bd88b473e1231c147b8b
pashwiniis <pashwini684@gmail.com>
2022-01-03 15:14:45 +0530

    Create a Address Book Table and stored the atttributes

--- UC1Query.sql
+++ UC1Query.sql
@@ -1,1 +1,17 @@
-﻿create database AddressBookService
+﻿
+
+-----UC1----------
+create database AddressBookService
+use AddressBookService
+------UC2-----------
+create table AddressBook(
+id int identity(1,1) not null,
+  firstName varchar(100) ,
+  lastName varchar(100),
+  address varchar(200),
+  city varchar(100),
+  state varchar(100),
+  zip int,
+  phoneNumber varchar(300),
+  email varchar(300)
+)
