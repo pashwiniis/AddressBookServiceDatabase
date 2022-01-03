d67d2b7ca1678e3535004275af1b735f8142f8db
pashwiniis <pashwini684@gmail.com>
2022-01-03 18:02:46 +0530

    retrive name of entries in sorted manner given by city

--- UC1Query.sql
+++ UC1Query.sql
@@ -20,11 +20,17 @@ select * from AddressBook
 insert into AddressBook values('Ashwini','Patil','102-Ramnagar','Dhule','Maharashtra',421502,'99975872889','pashwini684@hotmail.com');
 insert into AddressBook values('Sagar','Patil','103-shreenagar','Amalner','Maharashtra',534502,'99975872889','megha23@hotmail.com');
 insert into AddressBook values('Anvi','devare','201-chopda','jalgaon','kerala',675432,'9234512345','Anvi567@gmail.com');
+insert into AddressBook values('prajkta','chavhan','201-abc','Dhule','kerala',675432,'5678954321','abcd@gmail.com');
+insert into AddressBook values('Rakesh','wagh','201-krishnahall','Dhule','kerala',675432,'6754890987','djahdjhf@gmail.com');
 ------UC4-------------
 UPDATE AddressBook SET phoneNumber = '8765904567' WHERE firstName = 'Sagar';
 -------UC5---------------
 DELETE FROM AddressBook WHERE firstName ='Anvi';
+
 -------UC6--------------
 SELECT * FROM AddressBook WHERE city = 'Dhule' and state = 'Maharashtra';
 ---------UC7---------------
 SELECT COUNT(firstName) FROM AddressBook WHERE city = 'Amalner' and state = 'Maharashtra';
+---------UC8----------------
+
+SELECT * FROM AddressBook where city = 'Dhule' ORDER BY FirstName;
